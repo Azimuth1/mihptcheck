32117f1a5611baa77e73d6d15fccb61a7cf93f59
Jason Dalton <jason.dalton@azimuth1.com>
2018-01-25 17:00:12 -0500

    Update file calc code with new calls

--- R/mip_calc.R
+++ R/mip_calc.R
@@ -1,22 +1,22 @@
 mip_calc <- function(mipfile, water_level, ...){
 
     if(substring(tolower(mipfile), nchar(mipfile)-3) != ".zip"){
-        stop('Uploaded data needs to be .zip file. ');
+      stop('Uploaded data needs to be a .zip file. ');
     }
 
     options(stringsAsFactors = FALSE)
 
-    mhp_filename <- paste0(substr(basename(mipfile),0,nchar(basename(mipfile))-8),".mhp")
-    mip_file_data <- read.table(unz(mipfile, mhp_filename), header=T, quote="\"", sep="\t", na.strings = "n/a", row.names=NULL)
+    mhp_filename <- paste0(substr(basename(mipfile),0,nchar(basename(mipfile))-4),".mhp")
+    data <- read.table(unz(mipfile, mhp_filename), header=T, quote="\"", sep="\t", na.strings = "n/a", row.names=NULL)
 
-    col_names <- c( "Depth (ft)",	
-                    "EC (mS/m)",	
-                    "ROP (ft/min)",	
-                    "Temp. Min (deg C)",	
-                    "Temp. Max (deg C)",	
-                    "MIP Pressure (psi)",	
-                    "MIP Flow (mL/min)",	
-                    "Depth (m)",	
+    col_names <- c( "Depth (ft)",
+                    "EC (mS/m)",
+                    "ROP (ft/min)",
+                    "Temp. Min (deg C)",
+                    "Temp. Max (deg C)",
+                    "MIP Pressure (psi)",
+                    "MIP Flow (mL/min)",
+                    "Depth (m)",
                     "ROP (m/min)",
                     "MIP Pressure (kPa)",
                     "Detector 1 Min (uV)",
@@ -44,7 +44,41 @@ mip_calc <- function(mipfile, water_level, ...){
                     "HPT Line Press. Max (kPa)",
                     "HPT Screen Depth (ft)")
 
-    colnames(mip_file_data) <- col_names
+    colnames(data) <- col_names
+
+    waterlevels<-NULL
+    water_level <- as.numeric(water_level)
+
+    chop<-function(x,d){
+      tail(head(x,-d),-d)
+    }
+
+    chopmiddle<-function(x,d){
+      tail(head(x,length(x)/2),length(x)*0.4)
+    }
+    #d<-as.numeric(chopmiddle(data[,"Depth (ft)"],20))
+    #p<-chopmiddle(data[,"HPT Press. Avg (psi)"],20)
+    d<-as.numeric(data[,"Depth (ft)"])
+    p<-as.numeric(data[,"HPT Press. Avg (psi)"])
+    p_grad <- 0.44
+
+    plot(d,p,
+         type='l',
+         col='black',
+         xlab="Depth (ft)",
+         ylab="Pressure (PSI)",
+         ylim=c(0,40)
+         )
+    #abline(min(p),0,col="green")
+    grid()
+
+    p_c<-p
+    belowwater<-which(d>water_level)
+    p_c[belowwater]<-p[belowwater]-((d[belowwater]-water_level)*p_grad)
+
+    EstK<-0.00745*log10(data[,"HPT Flow Avg (mL/min)"]/p_c)  #cm/sec
+
+    mip_file_data<-cbind(data,p_c,EstK)
 
     return(mip_file_data)
 
