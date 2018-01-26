03654a5f612f18ff64ece39ef353bd6947cd156d
Jason Dalton <jason.dalton@azimuth1.com>
2018-01-26 13:11:42 -0500

    typo

--- R/mip_data_plot.R
+++ R/mip_data_plot.R
@@ -79,7 +79,7 @@ tryCatch({
       coord_flip()  +
       scale_x_reverse()
 
-    plot_grid(p1, p2 align = "h", ncol = 2, rel_widths = c(3/4, 1/4))
+    plot_grid(p1, p2, align = "h", ncol = 2, rel_widths = c(3/4, 1/4))
 
 
 }, error = function(e){
