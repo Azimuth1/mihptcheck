427956653e1765761d286c31f3d77de41b86eb96
Jason Dalton <jason.dalton@azimuth1.com>
2018-01-26 12:52:28 -0500

    Fix error loading couplet package

--- R/mip_data_plot.R
+++ R/mip_data_plot.R
@@ -78,7 +78,8 @@ tryCatch({
       coord_flip()  +
       scale_x_reverse()
 
-    plot_grid(p1, p2 align = "h", ncol = 2, rel_widths = c(3/4, 1/4))
+    #plot_grid(p1, p2 align = "h", ncol = 2, rel_widths = c(3/4, 1/4))
+    p1
 
 }, error = function(e){
 
