abbb17d349511eeec9ca0806a25d2599e6b13145
RobertSellers <robert.sellers@azimuth1.com>
2018-01-26 23:41:05 -0500

    ggplot fixes

--- R/mip_data_plot.R
+++ R/mip_data_plot.R
@@ -74,9 +74,9 @@ mip_data_plot <- function(mipfile, water_level, plotting){
         scale_x_reverse() +
         theme(legend.position="left")+
         geom_vline(aes(xintercept = water_level), linetype = "dashed", colour="blue",show.legend=FALSE)+
-        geom_text(aes(x=water_level, label="\nDepth to Water (ft)", y=20), colour="blue", angle=0, text=element_text(size=11))+
-        labs(fill="Legend")
+        geom_text(aes(x=water_level, label="\nDepth to Water (ft)", y=0), colour="purple", angle=0, text=element_text(size=11))
 
+        p1 <- p1 + guides(fill=guide_legend(title="Legend"))
 
         p2<-ggplot(data, aes(x = data$"Depth (ft)")) +
           geom_line(aes(y=data$"Est K (cm/sec)", color = "orange"),linetype = "longdash") +
