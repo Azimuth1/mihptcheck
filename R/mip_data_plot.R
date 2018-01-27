8a15ec1cb66ca08ee1b88be546b24055420ae05b
RobertSellers <robert.sellers@azimuth1.com>
2018-01-26 22:06:27 -0500

    plot test

--- R/mip_data_plot.R
+++ R/mip_data_plot.R
@@ -60,20 +60,23 @@ mip_data_plot <- function(mipfile, water_level, plotting){
     tryCatch({
 
       plot_title = substr(basename(mipfile),0,nchar(basename(mipfile))-8)
-        p1<-ggplot(data, aes(x = data$"Depth (ft)")) +
-          geom_line(aes(y = data$"Hydrostatic Pressure (psi)", color = "Hydrostatic Pressure"), linetype = "longdash") +
-          geom_line(aes(y = data$"HPT Press. Avg (psi)", color="HPT Press. Avg (psi)")) +
-          geom_line(aes(y = data$"Corr HPT Press (psi)", color = "Corrected Pressure")) +
-          geom_vline(aes(xintercept = water_level, color = "Depth to Water"), linetype = "dashed") +
-          scale_color_manual(values = c("red","lightblue","black","blue" )) +
-          scale_y_continuous(breaks=seq(0,max(data$"HPT Press. Avg (psi)",na.rm=TRUE),10)) +
-          scale_x_continuous(breaks=seq(0,max(data$"Depth (ft)",na.rm=TRUE),10)) +
-          ggtitle(basename(mipfile)) +
-          xlab("Depth (ft)") +
-          ylab("Pressure (psi)") +
-          coord_flip()  +
-          scale_x_reverse() +
-          theme(legend.position="left")
+      p1<-ggplot(data, aes(x = data$"Depth (ft)")) +
+        geom_line(aes(y = data$"Hydrostatic Pressure (psi)", color = "Hydrostatic Pressure"), linetype = "longdash") +
+        geom_line(aes(y = data$"HPT Press. Avg (psi)", color="HPT Press. Avg (psi)")) +
+        geom_line(aes(y = data$"Corr HPT Press (psi)", color = "Corrected Pressure")) +
+        geom_vline(aes(xintercept = water_level, color = "Depth to Water"), linetype = "dashed") +
+        # scale_color_manual(values = c("red","lightblue","black","blue" )) +
+        scale_color_manual(values = c("red","lightblue","black","blue" ),
+                           guide = guide_legend(override.aes = list(
+                             linetype = c("solid","solid", "solid", "dashed")))) +
+        scale_y_continuous(breaks=seq(0,max(data$"HPT Press. Avg (psi)",na.rm=TRUE),10)) +
+        scale_x_continuous(breaks=seq(0,max(data$"Depth (ft)",na.rm=TRUE),10)) +
+        ggtitle(basename(mipfile)) +
+        xlab("Depth (ft)") +
+        ylab("Pressure (psi)") +
+        coord_flip()  +
+        scale_x_reverse() +
+        theme(legend.position="left")
 
         p2<-ggplot(data, aes(x = data$"Depth (ft)")) +
           geom_line(aes(y=data$"Est K (cm/sec)", color = "orange"),linetype = "longdash") +
